32cbb6686d2b01a4bf0cd2cd9f652f90c1416424
Alexis <alexis.monier@etu.univ-lyon1.fr>
2025-12-14 19:32:21 +0100

    commentary for Player class

--- player_class.py
+++ player_class.py
@@ -2,19 +2,19 @@ import numpy as np
 class Player:
     def __init__(self, class_nb: float, ID: str):
         self.id = ID
-        self.value = self.set_value(class_nb)
+        self.value = self.set_value(class_nb) #value
         self.candidate = 0 #value
         self.candidate_id = "0" #id
         self.partner = 0 #value
         self.partner_id = "0" #id
         self.courtship_timer = 0
         self.mating = "waiting"
-        self.accept_candidate = True
+        self.accept_candidate = True #contain the choice of the player of accept a candidate partner or not
 
     def __repr__(self):
         return f"Joueur({self.id}, affichage={self.display_value:.2f}, classe={self.value_class})"
 
-    def player_info(self,visibility_dict):
+    def player_info(self,visibility_dict): #give the player information in the form of a dictionnary
         change = self.accept_candidate
         if self.candidate_id == self.partner_id:
             change = False
@@ -33,6 +33,10 @@ class Player:
         return player_info
 
     def build_class_thresholds(self,nb_classes: int) -> list[tuple[int, float]]:
+        #define N classes with (1 class by tuple) of reproductive values
+        #each tuple contain :
+        #a reproductive value for individuals (between 1 and N)
+        #the upper limit for a random number to correspond with this class (the lower limit is the upper limit of the preceding class)
         if nb_classes < 1:
             raise ValueError("nb_classes doit être >= 1")
         step = 1.0 / nb_classes
@@ -42,19 +46,22 @@ class Player:
         return thresholds
 
     def value_to_class(self,v: float, thresholds: list[tuple[int, float]]) -> int:
+        #receive a random number and return the value of the corresponding classe of values
         if not (0.0 <= v <= 1.0):
             raise ValueError("v doit être dans [0,1]")
         for cls, sup in thresholds:
+            #search the class corresponding to the random value and return the corresponding value
+            #return 1 if no corresponding class is found
             if v <= sup:
                 return cls
         return 1
     
-    def set_value(self,nb_class):
-        beta_value = np.random.beta(3,3)
+    def set_value(self,nb_class): #set a value to a player
+        beta_value = np.random.beta(3,3) #randow draw with a beta distribution close to a normal distribution
         #print(beta_value)
-        thresholds = self.build_class_thresholds(nb_class)
+        thresholds = self.build_class_thresholds(nb_class) #define the classes (value and thresold of each class)
         #print(thresholds)
-        class_value = self.value_to_class(beta_value,thresholds)
+        class_value = self.value_to_class(beta_value,thresholds) #give the reproductive value of the corresponding class
         #print(class_value)
         return class_value
     
